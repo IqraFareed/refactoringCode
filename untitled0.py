2aa2a388650bccab2fbcf01fa1ce02b9737c6097
IqraFareed <56933664+IqraFareed@users.noreply.github.com>
2019-11-28 23:02:05 -0800

    1st version

--- untitled0.py
+++ untitled0.py
@@ -1,108 +1,108 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 15 10:17:51 2019
-
-@author: nooru
-"""
-
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 15 06:27:04 2019
-
-@author: nooru
-"""
-
 import re
 
-
-
-
 def identifier(user):
-    rgx='^[A-Za-z_]+[A-Za-z0-9]*$'
+    rgx=r"^[a-zA-Z_]+(_)[a-zA-Z0-9]$"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is Identifier*"
+    else:
+        res="NOT a Identifier."
+    return res
 
 def digit(user):
     rgx=r"^[0-9]+$"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is Digit*"
+    else:
+        res="NOT a Digit."
+    return res
 
 def statement(user):
-    rgx='^(^(int|double|float|String|boolean|char)$)(^[A-Za-z_]+[A-Za-z0-9]*$)\s*[0-9]+;$'
+    rgx=r"^(int|float|string)\s+[a-zA-Z]+[0-9]\s(=|==|!=|<=|>=|<|>)\s*[a-zA-Z0-9]+;$"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is Statement*"
+    else:
+        res="NOT a Statement."
+    return res
 
 def operator(user):
-    rgx=r"(&&|\|\|+|-|*)"
+    rgx=r"(&&|\|\|)"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is Operator*"
+    else:
+        res="NOT a Operator."
+    return res
 
 def dataType(user):
-    rgx=r"^(int|double|float|String|boolean|char)$"
+    rgx=r"int|double|float|String|boolean|char"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is DataType*"
+    else:
+        res="NOT a DataType."
+    return res
 
 def forLoop(user):
     rgx=r"^for\s*\(\s*int\s*[a-zA-Z]+\s*=[0-9]+;\s*[a-zA-Z]+(<=|>=|<|>)[0-9]+;\s*[a-zA-Z++--]+\)$"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is For Loop*"
+    else:
+        res="NOT a For Loop."
+    return res
 
 def foreachLoop(user):
     rgx=r"^for\s*\((int|char|string)\s*[a-zA-Z]+\s*(:)\s*[a-zA-Z]+\)$"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is Foreach Loop*"
+    else:
+        res="NOT a Foreach Loop."
+    return res
 
 def whileLoop(user):
     rgx=r"^while\s*\([a-zA-Z]+(<=|>=|==|<|>|!=)\s[a-zA-Z0-9]\)\{\s\}$"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is While Loop*"
+    else:
+        res="NOT a While Loop."
+    return res
 
 def doWhileLoop(user):
     rgx=r"^do\{\s*\}\s*while\s*\([a-zA-Z]+(<=|>=|==|<|>|!=)\s[a-zA-Z0-9]*\)$"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is Do While Loop*"
+    else:
+        res="NOT a Do While Loop."
+    return res
 
 def ifStatement(user):
     rgx=r"^if\s*\([a-zA-Z]+(==|=|<=|>=|<|>)[a-zA-Z0-9]\s*(&&|\|\|)\s[a-zA-Z](==|=|<=|>=|<|>)[a-zA-Z0-9]\s\)*$"
     flg=re.match(rgx, user)
-    return flg
+    if(flg):
+        res="*It is If Statement*"
+    else:
+        res="NOT a If Statement."
+    return res
 
 def result(user):
+    print(identifier(user))
+    print(digit(user))
+    print(statement(user))
+    print(operator(user))
+    print(dataType(user))
+    print(forLoop(user))
+    print(foreachLoop(user))
+    print(whileLoop(user))
+    print(doWhileLoop(user))
+    print(ifStatement(user))
     
-    if(identifier(user)!=None):
-        print("It is an identifier")
-    
-    elif(digit(user)!=None):
-        print("It is a digit")
         
-    elif(statement(user)!=None):
-        print("It is a statement")
-    
-    elif(operator(user)!=None):
-        print("It is an operator")
-        
-    elif(dataType(user)!=None):
-        print("It is a data type")
-        
-    elif(forLoop(user)!=None):
-        print("It is a for loop")
-        
-    elif(foreachLoop(user)!=None):
-        print("It is a for each loop")
-        
-    elif(whileLoop(user)!=None):
-        print("It is a while loop")
-        
-    elif(doWhileLoop(user)!=None):
-        print("It is a do while loop")
-        
-    elif(ifStatement(user)!=None):
-        print("It is an if statement")
-        
-    else:
-        print("You are wrong")
-            
-    
 
 x=1
 print("**LEXICAL ANALYZER**")
@@ -120,3 +120,4 @@ while(x==1):
         x=1
     else:
         x=2
+
